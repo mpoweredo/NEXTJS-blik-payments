9b0276635287d25b2cd31a106915d4b5e8d8b3ba
mpoweredo <mpow3r1337@gmail.com>
2022-09-22 20:47:58 +0200

    :lipstick: Added own color to tailwind :lipstick:

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
         './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+          colors: {
+            bgDark: '#151515'
+          }
+        },
     },
     plugins: [],
 }
